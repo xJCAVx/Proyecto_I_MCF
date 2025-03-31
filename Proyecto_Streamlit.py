2296ee21d14341b8fcc32a0521e5f282d5248211
320283442 <josuecarlos@ciencias.unam.x>
2025-03-30 20:46:40 -0600

    cambios

--- Proyecto_Streamlit.py
+++ Proyecto_Streamlit.py
@@ -46,53 +46,45 @@ if activo_seleccionado:
 # c) --------------------------------------------------------------------------------
 # Calculo de VAR y ES con distintos métodos para múltiples valores de alpha
 
-def calcular_var_es(df):
-    NCS = [0.95, 0.975, 0.99]
-    resultados =  pd.DataFrame(columns=['VaR (Normal)', 'ES (Normal)','VaR (t-Student)','VaR (Histórico)', 'VaR (Monte Carlo)', 'ES (Normal)', 'ES (t-Student)', 'ES (Histórico)', 'ES (Monte Carlo)'])
-    df_size = len(df)
+    def calcular_var_es(df):
+        NCS = [0.95, 0.975, 0.99]
+        resultados =  pd.DataFrame(columns=['VaR (Normal)', 'ES (Normal)','VaR (t-Student)','ES (t-Student)','VaR (Histórico)', 'ES (Histórico)', 'VaR (Monte Carlo)', 'ES (Monte Carlo)'])
+        df_size = len(df)
+        i=0
+        for NC in NCS:
+            mean = df.mean()
+            stdev = df.std()
 
-    for NC in NCS:
-        mean = df.mean()
-        stdev = df.std()
+            #VaR Paramétrico(Normal)
+            VaR_norm = norm.ppf(1-NC,loc=mean,scale=stdev)
+            #ES Paramétrico (Normal)
+            ES_norm = mean - (stdev * norm.pdf(norm.ppf(NC)) / (1 - NC))
 
-        #VaR Paramétrico(Normal)
-        VaR_norm = norm.ppf(1-NC,loc=mean,scale=stdev)
-        #ES Paramétrico (Normal)
-        ES_norm = mean - (stdev * norm.pdf(norm.ppf(NC)) / (1 - NC))
+            #VaR Paramétrico (t-student)
+            nu = df_size - 1  # Grados de libertad
+            VaR_t = t.ppf(1 - NC, nu, loc=mean, scale=stdev)
+            #ES Paramétrico (t-student)
+            ES_t = mean - (stdev * t.pdf(t.ppf(NC, nu), nu) / (1 - NC))
 
-        #VaR Paramétrico (t-student)
-        nu = df_size - 1  # Grados de libertad
-        VaR_t = t.ppf(1 - NC, nu, loc=mean, scale=stdev)
-        #ES Paramétrico (t-student)
-        ES_t = mean - (stdev * t.pdf(t.ppf(NC, nu), nu) / (1 - NC))
+            # VaR Histórico
+            VaR_hist = df.quantile(1 - NC)
+            # ES Histórico
+            ES_hist = df[df <= VaR_hist].mean()
 
-        # VaR Histórico
-        VaR_hist = df.quantile(1 - NC)
-        # ES Histórico
-        ES_hist = df[df <= VaR_hist].mean()
+            # VaR Monte Carlo
+            sim_return = np.random.normal(mean, stdev, 100000)
+            VaR_MC = np.percentile(sim_return, 1 - NC)
+            # ES Monte Carlo
+            ES_MC = sim_return[sim_return <= VaR_MC].mean()
+            
+            resultados.loc[i] = [VaR_norm, ES_norm, VaR_t, ES_t, VaR_hist, ES_hist, VaR_MC, ES_MC]
+            i+=1
+        resultados.index = [f'{int(NC*100)}% de confianza' for NC in NCS]
+        return resultados
 
-        # VaR Monte Carlo
-        sim_return = np.random.normal(mean, stdev, 100000)
-        VaR_MC = np.percentile(sim_return, 1 - NC)
-        # ES Monte Carlo
-        ES_MC = sim_return[sim_return <= VaR_MC].mean()
-        
-        resultados[f'VaR (Normal) {NC}'] = VaR_norm
-        resultados[f'ES (Normal) {NC}'] = ES_norm
-        resultados[f'VaR (t-Student) {NC}'] = VaR_t
-        resultados[f'ES (t-Student) {NC}'] = ES_t
-        resultados[f'VaR (Histórico) {NC}'] = VaR_hist
-        resultados[f'ES (Histórico) {NC}'] = ES_hist
-        resultados[f'VaR (Monte Carlo) {NC}'] = VaR_MC
-        resultados[f'ES (Monte Carlo) {NC}'] = ES_MC
-    
-        resultados_df = pd.DataFrame(resultados, index=[f"{NC*100}%" for NC in NCS])
-
-    return resultados_df
-
-var_es_results = df_rendimientos.apply(calcular_var_es)
-
-print("Resultados de VaR y ES para cada activo con diferentes niveles de condianza y distribuciones:")
-print(var_es_results)
+    var_es_results = calcular_var_es(df_rendimientos)
+    st.dataframe(var_es_results)
+#print("Resultados de VaR y ES para cada activo con diferentes niveles de condianza y distribuciones:")
+#print(var_es_results)
 
 
