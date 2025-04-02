f34ba64944118a1696bb28ffb0951e2b3d2329eb
320283442 <josuecarlos@ciencias.unam.x>
2025-04-02 12:48:33 -0600

    ploty

--- Proyecto_Streamlit.py
+++ Proyecto_Streamlit.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
+import plotly.express as px
 import seaborn as sns 
 from scipy.stats import kurtosis, skew, norm, t
 
