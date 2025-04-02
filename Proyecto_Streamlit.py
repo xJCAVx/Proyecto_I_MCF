b17cda425abcd4bbab15c61f809d1a40b27d9ead
320283442 <josuecarlos@ciencias.unam.x>
2025-04-02 14:03:42 -0600

    c

--- Proyecto_Streamlit.py
+++ Proyecto_Streamlit.py
@@ -196,6 +196,7 @@ if activo_seleccionado:
         return TablaResultados  
 
     # Para ver la tabla
+    st.subheader("Tabla de violaciones")
     Tabla_violaciones=Calcular_Violaciones(df_rendimientos[activo_seleccionado] , df_var_es_rolling) # Los datos son porcentajes
     st.dataframe(Tabla_violaciones)
 
